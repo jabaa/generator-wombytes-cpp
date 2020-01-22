34bcc60c091d74be0e91c2ebec9aa59597048020
Thomas Sablik <thomas.sablik@wombytes.de>
2020-01-22 21:17:54 +0100

    Add unit test

--- test/test.js
+++ test/test.js
@@ -4,12 +4,23 @@ const path = require('path');
 const fs = require('fs');
 
 describe('App', () => {
-  it('generate a project', () => {
+  it('generate a project without argument', () => {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        projectName: 'projectName',
+        author: 'Author Name'
+      })
+      .then((tmpPath) => {
+        expect(fs.readdirSync(tmpPath)).to.eql(['projectName']);
+      });
+  });
+
+  it('generate a project with argument', () => {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withArguments(['projectName'])
       .withPrompts({author: 'Author Name'})
       .then((tmpPath) => {
         expect(fs.readdirSync(tmpPath)).to.eql(['projectName']);
       });
-  })
+  });
 });
