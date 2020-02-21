559daed184b7a56357feb3daf544d75162710779
Thomas Sablik <thomas.sablik@wombytes.de>
2020-02-21 02:22:27 +0100

    Add gsl

--- generators/app/templates/src/main.cpp
+++ generators/app/templates/src/main.cpp
@@ -1,5 +1,9 @@
+#include <gsl/span>
+#include <iostream>
 #include <cstdlib>
 
-int main() {
+int main(int argc, char *argv[]) {
+  gsl::span<char*> arguments(argv, argc);
+  std::cout << arguments[0] << '\n';
   return EXIT_SUCCESS;
 }
